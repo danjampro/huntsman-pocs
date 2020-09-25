1109bebe72aedefe84de308318c21c19d8990553
Dan <danjampro@sky.com>
2020-09-25 11:57:17 +1000

    load

---
+++ scripts/simulate_cpu_load.py
@@ -0,0 +1,16 @@
+"""Simple program to take up CPU and some RAM."""
+
+from multiprocessing import Pool
+import numpy as np
+
+
+def func():
+    while True:
+        data = np.random.normal(0, 1, shape=(5500, 2500))
+        data = data * data
+
+
+if __name__ == "__main__":
+
+    with Pool(4) as pool:
+        pool.map(func, [_ for _ in range(4)])
