4ca5845c8aa3f641c713dc9f7b9397bbad9aa536
Dan <danjampro@sky.com>
2020-10-09 16:30:49 +1100

    interim

--- docker/arm_device/startup.sh
+++ docker/arm_device/startup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sudo chown -R $PANUSER /home/$PANUSER/.ssh
 sudo chown -R $PANUSER $PANDIR/logs
-cd $HUNTSMAN_POCS
-python $HUNTSMAN_POCS/scripts/run_device.py
+cd ${HUNTSMAN_POCS}
+python setup.py develop
+python ${HUNTSMAN_POCS}/scripts/run_device.py
